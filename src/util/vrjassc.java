17b460a11bd516c768d833d26c2ed0cdb23f4cd1
Ruk33 <franco.montenegro.ruke@gmail.com>
2015-05-02 17:57:01 -0300

    first inject code into map try

--- src/util/vrjassc.java
+++ src/util/vrjassc.java
@@ -1,8 +1,10 @@
 package util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import de.peeeq.jmpq.JmpqEditor;
 import exception.CompileException;
 
 public class vrjassc {
@@ -10,13 +12,23 @@ public class vrjassc {
 	public static void main(String[] args) {
 		try {
 			Compile compile = new Compile();
-			PrintWriter writer = new PrintWriter("output.j", "UTF-8");
 			
-			for (String arg : args) {
-				writer.println(compile.runFromFile(arg));
-			}
+			File map = new File(args[0]);
+			JmpqEditor editor = new JmpqEditor(map);
 			
+			File code = File.createTempFile("war3map", ".wct");
+			File defaultCode = File.createTempFile("war3map", ".j");
+			File output = File.createTempFile("output", ".vrjass");
+			PrintWriter writer = new PrintWriter(output, "UTF-8");
+			
+			editor.extractFile("war3map.wct", code);
+			editor.extractFile("war3map.j", defaultCode);
+			
+			writer.println(compile.runFromFile(code.getAbsolutePath()));
 			writer.close();
+			
+			editor.injectFile(output, "war3map.j");
+			editor.close();			
 		} catch (CompileException ce) {
 			System.out.println(ce.getMessage());
 			System.exit(2);
@@ -24,6 +36,8 @@ public class vrjassc {
 			System.out.println(io.getMessage());
 			System.exit(1);
 		}
+		
+		System.exit(0);
 	}
 
 }
